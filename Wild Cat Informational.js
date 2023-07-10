cb5130074b7a62eb9b47192583895523b5b0480b
Erika Gibbons <105985132+ObsidianOverkill@users.noreply.github.com>
2023-07-10 10:44:10 -0400

    Add files via upload

---
+++ Wild Cat Informational.js
@@ -0,0 +1,46 @@
+//for lions//
+
+function newlionImg(){
+    document.getElementById("liontrans").src="mufasa (lion).jpg";
+
+}
+
+function oldlionImg(){
+    document.getElementById("liontrans").src="real lion.jpg";
+
+}
+
+//For Tigers//
+
+function newtigerImg(){
+    document.getElementById("tigertrans").src="tigress(tiger).jpg";
+
+}
+
+function oldtigerImg(){
+    document.getElementById("tigertrans").src="real tiger.jpg";
+
+}
+
+
+
+//for SL's//
+function newslImg(){
+    document.getElementById("sltrans").src="tai lung(snow leopard).jpg";
+
+}
+
+function oldslImg(){
+    document.getElementById("sltrans").src="real snow leopard.jpg";
+
+}
+
+
+//for jaguars//
+function newjagsImg(){
+    document.getElementById("jagstrans").src="Gia(jaguar).jpg";
+}
+
+function oldjagsImg(){
+    document.getElementById("jagstrans").src="real jaguar.jpg";
+}
